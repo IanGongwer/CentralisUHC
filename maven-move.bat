60da6863a8b2730a1e915f770745288873a590b7
Ian Gongwer <iangongwerpersonal@gmail.com>
2024-07-28 15:19:22 -0400

    Docker restarts in correct order and storms are cancelled

--- maven-move.bat
+++ maven-move.bat
@@ -1,6 +1,6 @@
 call mvn clean package
 cd ..
+docker compose down
 move /Y ".\CentralisUHC\target\centralisuhc-1.0.0.jar" ".\server\plugins\"
 cd CentralisUHC
-docker compose down
 docker compose up -d
--- src/main/java/com/iangongwer/listeners/Weather.java
+++ src/main/java/com/iangongwer/listeners/Weather.java
@@ -2,12 +2,24 @@ package com.iangongwer.listeners;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.weather.LightningStrikeEvent;
+import org.bukkit.event.weather.ThunderChangeEvent;
 import org.bukkit.event.weather.WeatherChangeEvent;
 
 public class Weather implements Listener {
 
 	@EventHandler
 	public void onWeatherChange(WeatherChangeEvent event) {
+		event.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onThunderChange(ThunderChangeEvent event) {
+		event.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onLightingStrike(LightningStrikeEvent event) {
 		event.setCancelled(true);
 	}
 
